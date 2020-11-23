b4f1c1187589ebd484bb2bc5c620e5fb6e0b61c9
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2020-11-23 18:21:41 +0700

    thrice

---
+++ dist/js/admin/recover.js
@@ -0,0 +1,20 @@
+$(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+  $(".toggle-password1").click(function() {
+
+    $(this).toggleClass1("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
--- dist/js/scriptAngularJs/recoverController.js
+++ dist/js/scriptAngularJs/recoverController.js
@@ -8,17 +8,15 @@ app.controller('recoverController', ['$scope', function ($scope) {
 
 	$scope.recoverSubmit = function (isValid) {
 		console.log($scope.recoverData);
-        if($scope.recoverData.password !== $scope.recoverData.confirmPassword){
-                toastr["error"](`mật khẩu không khớp`);
-                return 0;
-        }
-        if($scope.recoverData.password.length<6){
-            toastr["error"](`dai hon 6`);
+        if($scope.recoverData.password != $scope.recoverData.confirmPassword){
+            toastr["error"](`mật khẩu không khớp`);
             return 0;
-
-
         }
-		if (isValid) {
+        if($scope.recoverData.password.length<6 || $scope.recoverData.password.length > 20 ){
+            toastr["error"](`Mật khẩu phải dài từ 6 đến 20 kí tự`);
+            return 0;
+        }
+		else {
             toastr["success"](`thay đổi mật khẩu thành công`);
 		}
 		
