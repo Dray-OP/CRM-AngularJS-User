fb210b07f714fd2012113ba61b037cb60a38bb64
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2020-12-04 18:10:06 +0700

    category user

--- pages/examples/demoDirectiveCategory.js
+++ pages/examples/demoDirectiveCategory.js
@@ -1,14 +1,21 @@
 var app = angular.module("myApp", []);
  
 var ctrl = app.controller("myCtrl", function($scope) {
-  $scope.dataSettingUser =
+  $scope.dataSettingUsers =
     [
-        {id:'1',    key:'userName', name: 'Tên đăng nhập',     value: 'Abc'},
-        {id:'2',    key:'name' ,    name: 'Họ tên',            value: 'Nguyễn Văn A'},
-        {id:'3',    key:'phone' ,   name: 'Số điện thoại',     value: '012345678'},
-        {id:'4',    key:'email',    name: 'Email',             value: '@com'}
+        {id:'1',    key:'userName', name: 'Tên đăng nhập', value: 'Abc',            type:'text' },
+        {id:'2',    key:'name' ,    name: 'Họ tên',        value: 'Nguyễn Văn A',   type:'text' },
+        {id:'3',    key:'phone' ,   name: 'Số điện thoại', value: 012345678,        type:'number' },
+        {id:'4',    key:'email',    name: 'Email',         value: 'holongnhat@com', type:'email' },
+        {id:'5',    key:'address',  name: 'Địa chỉ',       value: '',               type:'text' },
+        {id:'6',    key:'birdDay',  name: 'Ngày sinh',     value: '',               type:'date' }
     ];   
-    
+
+    // $scope.dataSettingUsers
+
+    $scope.filterDataSettingUsersHasValue   = $scope.dataSettingUsers.filter(x=>x.value!='');
+    $scope.filterDataSettingUsersEmptyValue = $scope.dataSettingUsers.filter(x=>x.value=='');
+
     $scope.dataSettingLanguage ={
         //lấy id bằng model
         oldModel:1,
@@ -19,16 +26,25 @@ var ctrl = app.controller("myCtrl", function($scope) {
             {id:3 , name: 'ChaiEn'}
         ]
     };
-    $scope.dataChange = '';
+    // dùng để filter
+    // $scope.filterList = function(item){
+    //     return item.value != '' ;
+    // }
+    // $scope.filterAdd = function(item){
+    //     return item.value == '' ;
+    // }
 
+    $scope.dataChange = '';
     // khi focus thì nhận dữ liệu vào dataChange
     $scope.outFocus = function(item){
         console.log(item);
         $scope.dataChange = item.value; 
     }
-    console.log($scope.dataSettingUser);
+
     $scope.cancelAction = function(item){
-        item.value = $scope.dataChange; 
+        if(!$scope.dataChange == ''){
+            item.value = $scope.dataChange; 
+        }
     }
     
     // edit language
@@ -36,42 +52,18 @@ var ctrl = app.controller("myCtrl", function($scope) {
         $scope.dataSettingLanguage.model = $scope.dataSettingLanguage.oldModel;
         console.log($scope.dataSettingLanguage.model)
     }
+
+    // console.log($scope.dataSettingUsers[item.id-1].value)
+    // delete user
+    $scope.confirmDelete = function(item) {
+        $scope.dataSettingUsers[item.id-1].value ='';
+        $scope.filterDataSettingUsersHasValue   = $scope.dataSettingUsers.filter(x=>x.value!='');
+        $scope.filterDataSettingUsersEmptyValue = $scope.dataSettingUsers.filter(x=>x.value=='');
+    }
+    $scope.confirmAdd = function(item) {
+        $scope.filterDataSettingUsersHasValue   = $scope.dataSettingUsers.filter(x=>x.value!='');
+        $scope.filterDataSettingUsersEmptyValue = $scope.dataSettingUsers.filter(x=>x.value=='');
+    }
+
+    
 });
-
-// Create a Directive named "myTodoList"
-// E: Element <my-todo-list todo-list="=" title="@">
-// app.directive("listCategoryUsers",function(){
-//     return {
-//         restrict: "E",
-//         replace : true,
-//         templateUrl:"./demoListCategory.html",
-//         scope: {
-//             text:'=',
-//             todoList: '=',
-//             title: '@'
-//         }, 
-//         controller: function($scope){
-//             $scope.dataUser = 'ffffa';
-//             // $scope.changeInput = function(item){
-//             //     $scope.dataUser = item.value; 
-                
-//             // }
-//             $scope.outFocus = function(){
-//                 // $scope.dataUser = item.value; 
-//                 console.log("addd"); 
-//             }
-//             $scope.cancelAction = function(item){
-//                 // item.value = $scope.dataUser; 
-//                 // console.log($scope.dataUser);
-//                 console.log("addd"); 
-//             }
-//         }
-//     }
-// });
-
-
-    // $scope.submitEdit = function (isValid) {
-    //     console.log($scope.dataSettingUser);
-    //     $scope.dataSettingUser[0].value = $scope.dataUser;
-    //     console.log($scope.dataUser);
-    // };
