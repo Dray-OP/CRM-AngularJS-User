4c72028e566c5b1103887b0cacfad031d2dcd306
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2021-01-08 18:19:34 +0700

    all 1/8

--- zDemoAngularJs/app.js
+++ zDemoAngularJs/app.js
@@ -1,13 +1,35 @@
 var app = angular.module('mainApp', []);
 app.controller('people', ['$scope', '$http', function ($scope,$http) {
 
-$http({
-	method: 'GET',
-	url: 'http://127.0.0.1:5500/zDemoAngularJs/database.json'
-  }).then(function(response) {
-	  $scope.persons = response.data.records
-	  console.log(response.data.records)
-	}, function (response) {
-		console.log('Lỗi rồi nè')
-	});
+	$scope.name = null;
+	$scope.age = null;
+	$scope.address = null;
+
+	$scope.postData = function(name, age, address) {
+
+		var data = {
+			name: name,
+			age: age,
+			address: address
+		}
+
+		// $http({
+		// 	method: 'POST',
+		// 	url: 'https://jsonplaceholder.typidode.com/post'
+		//   }).then(function(response) {
+		// 	  $scope.persons = response.data.records
+		// 	  console.log(response.data.records)
+		// 	}, function (response) {
+		// 		console.log('Lỗi rồi nè')
+		// 	});
+
+		$http.post("https://jsonplaceholder.typicode.com/post", JSON.stringify(data))
+			.then(function (response){
+				console.log(response);
+
+				if(response.data){
+
+				}
+			})
+	}
 }]);
